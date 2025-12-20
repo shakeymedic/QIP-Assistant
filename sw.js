97f45493fd17ef76cd42e12d6557ea8b5092e628
shakeymedic <shakeymedic@users.noreply.github.com>
2025-12-20 23:16:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,18 @@
-const CACHE_NAME = 'rcem-qip-v1';
+const CACHE_NAME = 'rcem-qip-v1.2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/lucide@latest',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation@3.0.1/dist/chartjs-plugin-annotation.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js',
   'https://cdn.jsdelivr.net/npm/mermaid@10.6.1/dist/mermaid.min.js',
-  'https://cdn.jsdelivr.net/npm/pptxgenjs@3.12.0/dist/pptxgen.bundle.js'
+  'https://cdn.jsdelivr.net/npm/pptxgenjs@3.12.0/dist/pptxgen.bundle.js',
+  'https://www.transparenttextures.com/patterns/cubes.png'
 ];
 
 self.addEventListener('install', (e) => {
