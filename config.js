5e08fa316220ba944f2e8e96a874c6756c6cc905
shakeymedic <shakeymedic@users.noreply.github.com>
2026-01-05 16:35:02 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdu73Xb8xf4tJU4RLhJ82ANhLMI9eu0gI",
+    authDomain: "rcem-qip-app.firebaseapp.com",
+    projectId: "rcem-qip-app",
+    storageBucket: "rcem-qip-app.firebasestorage.app",
+    messagingSenderId: "231364231220",
+    appId: "1:231364231220:web:6b2260e2c885d40ecb4a61",
+    measurementId: "G-XHXTBQ29FX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
