df4afe56f28e9d3d8e640453659bc4c05189ff8c
Jake Turner <jaketurner2503@gmail.com>
2026-08-19 11:13:33 +0000

    Bump SW cache to v3.18.1 to force-refresh cached learn.js after syntax fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'rcem-qip-v3.18.0';
-const STATIC_CACHE = 'rcem-qip-static-v3.18.0';
-const DYNAMIC_CACHE = 'rcem-qip-dynamic-v3.18.0';
+const CACHE_NAME = 'rcem-qip-v3.18.1';
+const STATIC_CACHE = 'rcem-qip-static-v3.18.1';
+const DYNAMIC_CACHE = 'rcem-qip-dynamic-v3.18.1';
 
 const STATIC_ASSETS = [
     '/',
